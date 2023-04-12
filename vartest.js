edb8aed831f2ac0dc62d4a9bc671d74e6fe00bfe
kristersg <kristers-martins.gelass@edu.rtu.lv>
2023-04-12 14:52:56 +0300

    Removed unneeded files

--- vartest.js
+++
@@ -1,9 +0,0 @@
-const variables = new Map();
-
-variables.set('x', 28);
-variables.set('vards', 'Kristers');
-
-console.log(variables);
-
-console.log(typeof variables.get('x'));
-console.log(typeof variables.get('vards'));
