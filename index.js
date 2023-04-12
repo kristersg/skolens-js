54640913ddc1da657bf1097c6b9a3d9d487471e3
kristersg <kristers-martins.gelass@edu.rtu.lv>
2023-04-12 15:07:43 +0300

    Add option to specify source code file in command args

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ import SkolensLexer from './lib/SkolensLexer.js';
 import SkolensParser from './lib/SkolensParser.js';
 import SkolensInterpreter from './SkolensInterpreter.js';
 
-// const input = 'teikt 7 * (2  + 2);';
-const data = fs.readFileSync('test.sk', { encoding: 'utf8' });
+const args = process.argv.slice(2);
+if (args.length == 0) {
+  console.error('Kļūda: nav norādīts programmas pirmkoda fails!');
+  console.error('Izmantošana:  npm start <cels/uz/failu.sk>');
+  process.exit(1);
+}
+
+if (!fs.existsSync(args[0])) {
+  console.error(`Kļūda: norādītais fails netika atrasts! (${args[0]})`);
+  process.exit(1);
+}
+const data = fs.readFileSync(args[0], { encoding: 'utf8' });
 
 const chars = new antlr4.InputStream(data);
 const lexer = new SkolensLexer(chars);
